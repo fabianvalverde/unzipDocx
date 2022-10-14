bf3f61de7679c033fe95f6c096e3d2d6e124a9b6
fabianvalverde <110683318+fabianvalverde@users.noreply.github.com>
2022-10-13 23:54:55 -0600

    Simple code to unzip docx

---
+++ unzipDocx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Compression;
+using System.Runtime.ConstrainedExecution;
+
+namespace unzipDocx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your .docx file path:");
+            String path = Console.ReadLine();
+            Console.WriteLine("Write the path where you want your file:");
+            String destinationPath = Console.ReadLine();
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(path))
+                {
+                    Console.WriteLine(archive.ToString());
+                    archive.ExtractToDirectory(destinationPath + "unzip", true);
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine($"failed {e}");
+            }
+
+        }
+    }
+}
